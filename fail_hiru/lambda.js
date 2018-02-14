a36a4351add1026c8d7556ada3ecc22b7b4f738d
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-14 17:21:38 +0530

    db added

--- fail_hiru/lambda.js
+++ fail_hiru/lambda.js
@@ -1,6 +1,18 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	ddb.scan({
+		TableName: 'Hiru_DB',
+		ExpressionAttributeValues: {},
+		FilterExpression: '',
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
